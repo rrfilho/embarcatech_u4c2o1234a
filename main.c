22cfb80af10fa7d2fa422a9552b7cf6c887d7946
HOST-PC\Host <rrfilho2@hotmail.com>
2025-01-19 17:38:39 -0300

    - ajuste main

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
-#include "buzzer.h"
 
-int main() {
-    start_buzzer(); 
+
+
+int main()
+{
+    stdio_init_all();
 
     while (true) {
-        char key = keypad_leitura();  // Lê a tecla pressionada do teclado ==== FUNÇÃO Keypad_leitura() apagada
-        if (key) {
-            ativa_buzzer(key);  // Ativa o buzzer na tecla pressionada
-        }
-        sleep_ms(1000);  
+        printf("Hello, world!\n");
+        sleep_ms(1000);
     }
-
-    return 0;
 }
