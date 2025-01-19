d1364f9afce331f0ab15711755c51c62db37d703
BrenoPampone <136191058+BrenoPampone@users.noreply.github.com>
2025-01-19 13:29:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include "pico/stdlib.h"
 #include "buzzer.h"
-#include "keypad.h"
 
-int main(){
-  start_gpio();  // Inicializa os pinos do teclado
+int main() {
+    start_buzzer(); 
 
-  while(true){
-    char key = keypad_leitura();  // Lê a tecla pressionada
-    if(key){
-       teclado_opcao(key);  // Chama a função do buzzer com base na tecla pressionada
+    while (true) {
+        char key = keypad_leitura();  // Lê a tecla pressionada do teclado  ========== KEYPAD_LEITURA ESTA COMENTADO NO BUZZER.H
+        if (key) {
+            ativa_buzzer(key);  // Ativa o buzzer na tecla pressionada
+        }
+        sleep_ms(1000);  
     }
-    sleep_ms(1000);  // Espera 1 segundo antes de verificar a próxima tecla
-  }
-  return 0;
+
+    return 0;
 }
